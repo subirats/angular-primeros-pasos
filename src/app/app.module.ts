adb510071249f1294ee6ac34db67f049e9bd5621
subirats <lluis.subiratsballesteros@cieep.com>
2024-10-02 12:01:26 +0200

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesModule } from './heroes/heroes.module';
+import {CounterModule} from './counter/counter.module';
+import { DbzModule } from './dbz/dbz.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CounterModule,
+    HeroesModule,
+    DbzModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent {
+
+  public name: string = 'ironman';
+  public age: number = 45; 
+
+  get capitalizeName():string{
+    return this.name.toLocaleUpperCase();
+  }
+
+  getHeroDescription(): string{
+    return `${this.name} - ${this.age}`
+  }
+
+  changeHero():void{
+    this.name = 'Spiderman';
+  }
+
+  changeAge():void{
+    this.age = 25; 
+  }
+
+  resetForm():void{
+    this.name = 'ironman';
+    this.age = 45; 
+
+    document.querySelectorAll('h1').forEach(element => {
+      element.innerHTML = '<h1> Desde angular</h1>'  
+    });
+   }
+}
